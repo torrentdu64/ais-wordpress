1d328c04e4225d3f8e3f4c1af08093cdbc557d59
admin <torrentdu64@gmail.com>
2020-06-10 17:35:40 +1200

    format copie/paste '

--- wp-config.php
+++ wp-config.php
@@ -46,16 +46,16 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
-    $db = parse_url($_ENV[`CLEARDB_DATABASE_URL`]);
-    define(‘DB_NAME’, trim($db[`path`],`/`));
-    define(‘DB_USER’, $db[`user`]);
-    define(‘DB_PASSWORD’, $db[`pass`]);
-    define(‘DB_HOST’, $db[`host`]);
-    define(‘DB_CHARSET’, ‘utf8’);
-    define(‘DB_COLLATE’, ‘’);
+if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
+    $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+    define('DB_NAME', trim($db['path'],'/'));
+    define('DB_USER', $db['user']);
+    define('DB_PASSWORD', $db['pass']);
+    define('DB_HOST', $db['host']);
+    define('DB_CHARSET', 'utf8');
+    define('DB_COLLATE', '');
 } else {
-    die(‘No Database credentials!’);
+    die('No Database credentials!');
 }
 
 /**#@-*/
